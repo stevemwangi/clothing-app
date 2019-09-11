7893d326111eeab0e116133db90e09a74087c804
stevemwangi <sttevemwangi@gmail.com>
2019-09-11 14:32:51 +0300

    added Redirect to the sign-in and up pages, added types to actions and reducers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -43,18 +43,19 @@ class App extends React.Component {
     <Switch>
         <Route exact path="/" component = {HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-        {/* <Route path="/signin" component={SignInAndSignUp} /> */}
-        {/* <Route 
-            exact 
-            path="/signin" 
-            render={() => 
-              this.props.currentUser ? (
-                <Redirect to="/" />
-                ) : (
-                <SignInAndSignUp />
-                ) }
-                /> */}
+        {/* <Route path='/signin' component={SignInAndSignUpPage} /> */}
+        <Route 
+        exact 
+        path="/signin" render={() =>
+          this.props.currentUser ? (
+            <Redirect to="/" />
+          ) : (
+            <SignInAndSignUpPage />
+          ) 
+        }
+      
+        />
+        
     </Switch>     
     </div>
   );
